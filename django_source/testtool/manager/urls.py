c63167c83b0017c522bad32fa9c7249b68a10ac0
Can Bal <canbal@gmail.com>
2013-03-01 20:07:59 -0800

    [bugfix] changes for removing Django 1.5 depracation warnings

--- django_source/testtool/manager/urls.py
+++ django_source/testtool/manager/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from testtool.manager.views import CreateTest, DisplayTest, EditTest, CreateTestInstance, DisplayTestInstance, EditTestInstance
 
 urlpatterns = patterns('testtool.manager.views',
--- django_source/testtool/registration/urls.py
+++ django_source/testtool/registration/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('testtool.registration.views',
     url(r'^login/$',           'custom_login',    { 'template_name':'testtool/registration/login.html' }, name='login'),
